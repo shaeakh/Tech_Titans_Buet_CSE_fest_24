deb7cdc1b0aef4958fe9cab079567868d4c53465
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 <nixon23@student.sust.edu>
2024-10-25 03:51:22 +0600

    getImage API's for gallery

--- asset_server/index.js
+++ asset_server/index.js
@@ -4,6 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
 const { saveImage } = require('./controllers/saveImage');
+const { getImagesByUser } = require('./controllers/getImagesByUser ');
+const { getImagesByActivity } = require('./controllers/getImagesByActivity');
+const { getImagesByTrip } = require('./controllers/getImagesByTrip');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +16,9 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/api/save-img',saveImage);
+app.get('/api/images/user/:userID',getImagesByUser);
+app.get('/api/images/trip/:tripID' ,getImagesByTrip);
+app.get('/api/images/activity/:activityID',getImagesByActivity);
 
 mongoose.connect(DBURL)
     .then(() => {
